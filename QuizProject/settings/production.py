0575582dc7612a32daf85b35e7a3f14781058730
b.amirov <amirovbobur52@gmail.com>
2024-01-20 19:48:39 +0500

    added docker file

--- QuizProject/settings/production.py
+++ QuizProject/settings/production.py
@@ -5,8 +5,12 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../db.sqlite3',
+    "default": {
+        "ENGINE": config("SQL_ENGINE"),
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_PASSWORD"),
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
     }
 }
